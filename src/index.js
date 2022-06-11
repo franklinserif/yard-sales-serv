2414a2fa1a40ab3e5087f99cc61265a5e780df27
franklin rodriguez <franklinserif@gmail.com>
2022-06-11 00:11:09 -0400

    cors config
    Implementing cors with options and whiteList

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+const whiteList = ['http://localhost:8080'];
+
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No authorized'));
+    }
+  },
+};
+
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('working');
