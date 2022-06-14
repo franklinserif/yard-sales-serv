a7bfe8c1d56d0f1c61c24f82212bc2c68434a8c4
franklin rodriguez <franklinserif@gmail.com>
2022-06-14 17:55:52 -0400

    customer schema for sequelize :construction:

---
+++ src/db/models/customer.model.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const { USER_TABLE } = require('./user.model');
+
+const CUSTOMER_TABLE = 'customers';
+
+const CustomerSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+  },
+
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+
+  lastName: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+
+  phone: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+
+  userId: {
+    field: 'user_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    unique: true,
+    references: {
+      model: USER_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  },
+};
