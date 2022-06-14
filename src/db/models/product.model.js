303c8be44530b6f4634a44b6c6a887b7f55cca9c
franklin rodriguez <franklinserif@gmail.com>
2022-06-14 18:12:25 -0400

    add categoryId field for product schema :construction:

--- src/db/models/product.model.js
+++ src/db/models/product.model.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes, Sequelize } = require('sequelize');
+const { CATEGORY_TABLE } = require('./category.model');
 
 const PRODUCT_TABLE = 'products';
 
@@ -28,6 +29,17 @@ const ProductSchema = {
   price: {
     allowNull: false,
     type: DataTypes.STRING,
+  },
+
+  categoryId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: CATEGORY_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
   },
 
   createdAt: {
