c1902611bca947eb7072c82d84cf1bd93fb04611
franklin rodriguez <franklinserif@gmail.com>
2022-06-16 05:26:54 -0400

    Auth Service :construction:

---
+++ src/services/auth.service.js
@@ -0,0 +1,9 @@
+/* eslint-disable class-methods-use-this */
+/**
+ * It contains all the class and he's methods
+ * for manage all the table information
+ * @module services/auth
+ */
+
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
