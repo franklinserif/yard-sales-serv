ad3fe6e6ba70aa7431858ff94039f870c5082a45
franklin rodriguez <franklinserif@gmail.com>
2022-06-16 05:43:14 -0400

    Auth service need to improve :wrench:

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -7,3 +7,89 @@
 
 const boom = require('@hapi/boom');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+
+const { config } = require('../config/config');
+const UserService = require('./user.service');
+
+const service = new UserService();
+
+/**
+ * This class will define all methods for  manage
+ * Authentication
+ */
+class AuthService {
+  /**
+   * It will verify if the user exist and if
+   * the password is the same
+   * @async
+   * @param {string} email
+   * @param {string} password
+   * @returns {Promise<Object>}
+   */
+  async getUser(email, password) {
+    const user = await service.findByEmail(email);
+
+    if (!user) throw boom.unauthorized();
+
+    const isMath = bcrypt.compare(password, user.password);
+
+    if (!isMath) throw boom.unauthorized();
+    // @ts-ignore
+    const { password: ps, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+  }
+
+  /**
+   * It will sign a token
+   * @param {Object} user
+   * @returns {Object}
+   */
+  signToken(user) {
+    const payload = {
+      sub: user.id,
+      role: user.role,
+    };
+
+    const token = jwt.sign(payload, config.jwtSecret);
+    return {
+      user,
+      token,
+    };
+  }
+
+  /**
+   * It will send an email
+   * @async
+   * @param {string} email
+   * @returns {Promise<Object>}
+   */
+  async sendMail(email) {
+    const user = await service.findByEmail(email);
+
+    if (!user) throw boom.unauthorized();
+
+    const transporte = nodemailer.createTransport({
+      host: config.emailHost,
+      secure: true,
+      port: 465,
+      auth: {
+        user: config.emailSender,
+        pass: config.emailPassword,
+      },
+    });
+
+    await transporte.sendMail({
+      form: config.emailSender,
+      to: `${user.email}`,
+      subject: 'Recovery password',
+      text: 'Hola',
+      html: '<p>Hola</p>',
+    });
+
+    return { message: 'mail send' };
+  }
+}
+
+module.exports = AuthService;
