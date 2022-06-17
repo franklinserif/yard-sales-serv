a301c387d1b389d8d71d017e5dbeee5a669ddf93
franklin rodriguez <franklinserif@gmail.com>
2022-06-16 22:24:39 -0400

    validator handler :construction:

---
+++ src/middlewares/validator.handler.js
@@ -0,0 +1,29 @@
+/**
+ * This module contains all middleware
+ * that validate data
+ * @module middlewares/validator
+ */
+
+const boom = require('@hapi/boom');
+
+/**
+ * It will validate if the data is correct
+ * dependin of the schema
+ * @param {Object} schema
+ * @param {string} property
+ * @returns {Function}
+ */
+function validatorHandler(schema, property) {
+  return (req, res, next) => {
+    const data = req[property];
+    const { error } = schema.validate(data, { abortEarly: false });
+
+    if (error) {
+      next(boom.badRequest(error));
+    } else {
+      next();
+    }
+  };
+}
+
+module.exports = validatorHandler;
