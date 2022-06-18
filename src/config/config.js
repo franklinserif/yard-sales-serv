a38e33093b1a67d961cb378dc958e302e2af2aa3
franklin rodriguez <franklinserif@gmail.com>
2022-06-17 22:53:11 -0400

    update comment in config/config :bulb:

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,23 @@
  * It will read all .env variable and make
  * available for the entire project
  * @module  config/config
+ * @requires dotenv
  */
 require('dotenv').config();
 
 /**
  * All .env variable
  * @type {Object}
+ * @constant
+ * @memberof config/config
+ * @property {string} env - enviroment default is DEV
+ * @property {number} serverPort - server's port
+ * @property {string} dbUrl - database's url connection
+ * @property {string} apiKey - apiKey for validation
+ * @property {string} jwtSecret - secret key for token sign
+ * @property {string} emailSender - email for from field
+ * @property {string} emailPassword - email password
+ * @property {string} emailHost - host email
  */
 module.exports = {
   env: process.env.ENV || 'DEV',
