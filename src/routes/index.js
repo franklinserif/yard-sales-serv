1bca4c786c22e519cfa86745cce2554593a39d20
franklin rodriguez <franklinserif@gmail.com>
2022-06-19 21:34:22 -0400

    routes/index added routers to the main app :construction:

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,21 @@
  * @module routes/index
  */
 
+/**
+ * Main api router for serving all routes
+ * @constant
+ */
 const express = require('express');
 
+/**
+ *
+ */
+const authRouter = require('./auth.router');
+const categoriesRouter = require('./categories.router');
+const customersRouter = require('./customers.router');
+const ordersRouter = require('./orders.router');
+const productsRouter = require('./products.router');
+const profileRouter = require('./profile.router');
 /**
  * Setup all routes and add o the main app
  * @param {Object} app
@@ -14,6 +27,12 @@ const express = require('express');
 function routeApi(app) {
   const router = express.Router();
   // Add all router here
+  router.use('/auth', authRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/customers', customersRouter);
+  router.use('/ordersRouter', ordersRouter);
+  router.use('/products', productsRouter);
+  router.use('/profile', profileRouter);
   app.use('/api/v1', router);
 }
 
